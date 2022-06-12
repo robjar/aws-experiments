9325acaa89ccdc8c5e170f97a82e3defbde61486
Robert Jarosz <robjar@wp.eu>
2022-06-12 16:00:09 +0200

    wip

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
     region = "us-east-1"
 }
-
-resource "aws_instance" "my-first-server" {
-  ami           = "ami-0747bdcabd34c712a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "ubuntu"
-  }
-}
